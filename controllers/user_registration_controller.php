b0a0333d05568fcf662cfe4774cba1bb9db864f5
VoiDeT <voidet@nightgen.com>
2010-09-05 18:48:53 +1000

    Generate activation code & inherit parent model

--- controllers/user_registration_controller.php
+++ controllers/user_registration_controller.php
@@ -2,11 +2,23 @@
 
 class UserRegistrationController extends AppController {
 
-	var $name = 'UserRegistration';
-	var $uses = array('User');
+	public $helpers = array('Form', 'Html');
+	public $name = 'UserRegistration';
+	public $uses = array('SignMeUp.UserRegistration');
+
+	public function beforeFilter() {
+		$this->Auth->allow('register');
+		return parent::beforeFilter();
+	}
 
 	public function register() {
-
+		if (!empty($this->data)) {
+			$this->UserRegistration->set($this->data);
+			if ($this->UserRegistration->validates()) {
+				$this->data['UserRegistration']['activation_code'] = $this->UserRegistration->generateActivationCode();
+				$this->UserRegistration->save($this->data, false);
+			}
+		}
 	}
 
 }
