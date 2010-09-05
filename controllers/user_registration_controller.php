f24224632dec73a6215d195bb99aa31ad4ceb165
VoiDeT <voidet@nightgen.com>
2010-09-05 19:16:12 +1000

    Added in user activation

--- controllers/user_registration_controller.php
+++ controllers/user_registration_controller.php
@@ -7,7 +7,7 @@ class UserRegistrationController extends AppController {
 	public $uses = array('SignMeUp.UserRegistration');
 
 	public function beforeFilter() {
-		$this->Auth->allow('register');
+		$this->Auth->allow('register', 'activate');
 		return parent::beforeFilter();
 	}
 
@@ -16,7 +16,26 @@ class UserRegistrationController extends AppController {
 			$this->UserRegistration->set($this->data);
 			if ($this->UserRegistration->validates()) {
 				$this->data['UserRegistration']['activation_code'] = $this->UserRegistration->generateActivationCode();
-				$this->UserRegistration->save($this->data, false);
+				if ($this->UserRegistration->save($this->data, false)) {
+					$this->redirect(array('action' => 'activate'));
+				}
+			}
+		}
+	}
+
+	public function activate($activation_code = '') {
+		if (!empty($activation_code) || !empty($this->data)) {
+			if (!empty($this->data)) {
+				$activation_code = $this->data['UserRegistration']['activation_code'];
+			}
+			$inactive_user = $this->UserRegistration->find('first', array('conditions' => array('activation_code' => $activation_code), 'recursive' => -1));
+			if (!empty($inactive_user)) {
+				$inactive_user['UserRegistration']['active'] = true;
+				$inactive_user['UserRegistration']['activation_code'] = null;
+				if ($this->UserRegistration->save($inactive_user)) {
+					$this->Session->setFlash('Thank you '.$inactive_user['UserRegistration']['username'].', your account is now active');
+					$this->redirect($this->Auth->loginAction);
+				}
 			}
 		}
 	}
