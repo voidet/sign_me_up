a5c78dc2c6a6e62df4d9dc22ff4c11a1afafc5e7
VoiDeT <voidet@nightgen.com>
2010-09-06 20:43:30 +1000

    Allow email sending with parameter overrides from parent

--- controllers/user_registration_controller.php
+++ controllers/user_registration_controller.php
@@ -1,14 +1,30 @@
 <?php
 
-class UserRegistrationController extends AppController {
+App::import('Controller', 'Users');
 
+class UserRegistrationController extends UsersController {
+
+	public $components = array('Email');
 	public $helpers = array('Form', 'Html');
 	public $name = 'UserRegistration';
 	public $uses = array('SignMeUp.UserRegistration');
 
 	public function beforeFilter() {
-		$this->Auth->allow('register', 'activate');
-		return parent::beforeFilter();
+		parent::beforeFilter();
+		if ($this->Auth->user()) {
+			$this->redirect($this->Auth->loginRedirect);
+		} else {
+			$this->Auth->allow('register', 'activate');
+		}
+	}
+
+	private function __setUpEmailParams() {
+		$this->Email->delivery = 'mail';
+		$this->Email->from = Configure::read('Site.email');
+		if (Configure::read('Site.email_layout')) {
+			$this->Email->layout = Configure::read('Site.email_layout');
+		}
+		$this->Email->sendAs = 'text';
 	}
 
 	public function register() {
@@ -16,10 +32,18 @@ class UserRegistrationController extends AppController {
 			$this->UserRegistration->set($this->data);
 			if ($this->UserRegistration->validates()) {
 				$this->data['UserRegistration']['activation_code'] = $this->UserRegistration->generateActivationCode();
-				if ($this->UserRegistration->save($this->data, false)) {
+				if ($this->UserRegistration->save($this->data, false) && $this->__sendSignupEmail($this->data['UserRegistration'])) {
 					$this->redirect(array('action' => 'activate'));
 				}
 			}
+		}
+	}
+
+	protected function __sendSignupEmail($userData) {
+		$this->__setUpEmailParams();
+		parent::__sendSignupEmail($userData);
+		if ($this->Email->send()) {
+			return true;
 		}
 	}
 
