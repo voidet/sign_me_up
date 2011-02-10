67d1ce63467d4d6311a85bbd0c23956706ce3511
Richard S <voidet@nightgen.com>
2011-02-10 21:03:09 +1000

    Added in the ability to create accounts without activation.

--- controllers/components/sign_me_up.php
+++ controllers/components/sign_me_up.php
@@ -3,11 +3,16 @@
 class SignMeUpComponent extends Object {
 
 	public $components = array('Session', 'Email', 'Auth');
+	public $defaults = array(
+		'activation_field' => 'activation_code',
+		'useractive_field' => 'active',
+	);
 	public $helpers = array('Form', 'Html');
 	public $name = 'SignMeUp';
 	public $uses = array('SignMeUp');
 
 	function initialize(&$controller, $settings = array()) {
+		$this->settings = array_merge($this->defaults, $settings);
 		$this->controller = &$controller;
 		if ($this->Auth->user()) {
 			$this->controller->redirect($this->Auth->loginRedirect);
@@ -28,56 +33,83 @@ class SignMeUpComponent extends Object {
 		$this->Email->sendAs = Configure::read('SignMeUp.type');
 		$this->Email->subject = str_replace('%username%', $user['username'], Configure::read('SignMeUp.subject'));
 		$this->Email->to = $user['username'].' <'.$user['email'].'>';
-		$this->Email->template = Configure::read('SignMeUp.template');
 		$this->Email->xMailer = Configure::read('SignMeUp.xMailer');
 		$this->controller->set(compact('user'));
 	}
 
 	public function register() {
 		if (!empty($this->controller->data)) {
+			extract($this->settings);
 			$model = $this->controller->modelClass;
 			$this->controller->loadModel($model);
 			$this->controller->{$model}->set($this->controller->data);
 			if ($this->controller->{$model}->validates()) {
-				$this->controller->data[$model]['activation_code'] = $this->controller->{$model}->generateActivationCode($this->controller->data);
-				if ($this->controller->{$model}->save($this->controller->data, false) && $this->__sendSignupEmail($this->controller->data[$model])) {
-					$this->controller->redirect(array('action' => 'activate'));
+				if (!empty($activation_field)) {
+					$this->controller->data[$model][$activation_field] = $this->controller->{$model}->generateActivationCode($this->controller->data);
+				}
+				if ($this->controller->{$model}->save($this->controller->data, false)) {
+					//If an activation field is supplied send out an email
+					if (!empty($activation_field)) {
+						$this->__sendActivationEmail($this->controller->data[$model]);
+						$this->controller->redirect(array('action' => 'activate'));
+					} else {
+						$this->__sendWelcomeEmail($this->controller->data[$model]);
+					}
+					$this->controller->redirect($this->Auth->loginAction);
 				}
 			}
 		}
 	}
 
-	protected function __sendSignupEmail($userData) {
+	protected function __sendActivationEmail($userData, $template) {
 		$this->__setUpEmailParams($userData);
+		$this->Email->template = Configure::read('SignMeUp.activation_template');
+		if ($this->Email->send()) {
+			return true;
+		}
+	}
+
+	protected function __sendWelcomeEmail($userData, $template) {
+		$this->__setUpEmailParams($userData);
+		$this->Email->template = Configure::read('SignMeUp.welcome_template');
 		if ($this->Email->send()) {
 			return true;
 		}
 	}
 
 	public function activate() {
-		if (!empty($this->controller->params['activation_code'])) {
-			$activation_code = $this->controller->params['activation_code'];
-		}
+		extract($this->settings);
+		//If there is no activation field specified, don't bother with activation
+		if (!empty($activation_field)) {
 
-		if (!empty($activation_code) || !empty($this->controller->data)) {
-			$model = $this->controller->modelClass;
-			$this->controller->loadModel($model);
-
-			if (!empty($this->controller->data)) {
-				$activation_code = $this->controller->data[$model]['activation_code'];
+			//Test for an activation code in the parameters
+			if (!empty($this->controller->params[$activation_field])) {
+				$activation_code = $this->controller->params[$activation_field];
 			}
 
-			$inactive_user = $this->controller->{$model}->find('first', array('conditions' => array('activation_code' => $activation_code), 'recursive' => -1));
-			if (!empty($inactive_user)) {
-				$this->controller->{$model}->id = $inactive_user[$model]['id'];
-				$data[$model]['active'] = true;
-				$data[$model]['activation_code'] = null;
-				if ($this->controller->{$model}->save($data)) {
-					$this->Session->setFlash('Thank you '.$inactive_user[$model]['username'].', your account is now active');
-					$this->controller->redirect($this->Auth->loginAction);
+			//If there is an activation code supplied, either in _POST or _GET
+			if (!empty($activation_code) || !empty($this->controller->data)) {
+				$model = $this->controller->modelClass;
+				$this->controller->loadModel($model);
+
+				if (!empty($this->controller->data)) {
+					$activation_code = $this->controller->data[$model][$activation_field];
 				}
-			} else {
-				$this->Session->setFlash('Sorry, that code is incorrect.');
+
+				$inactive_user = $this->controller->{$model}->find('first', array('conditions' => array($activation_field => $activation_code), 'recursive' => -1));
+				if (!empty($inactive_user)) {
+					$this->controller->{$model}->id = $inactive_user[$model]['id'];
+					if (!empty($useractive_field)) {
+						$data[$model][$useractive_field] = true;
+					}
+					$data[$model][$activation_field] = null;
+					if ($this->controller->{$model}->save($data)) {
+						$this->Session->setFlash('Thank you '.$inactive_user[$model]['username'].', your account is now active');
+						$this->controller->redirect($this->Auth->loginAction);
+					}
+				} else {
+					$this->Session->setFlash('Sorry, that code is incorrect.');
+				}
 			}
 		}
 	}
